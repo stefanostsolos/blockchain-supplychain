00885698e8200e91cfaeb9d0b35b1c7752a5e011
Stefanos Tsolos <stefant@hotmail.gr>
2023-06-14 18:04:20 +0300

    Update webapp

--- web-app/client/src/components/shipments-list.component.js
+++ web-app/client/src/components/shipments-list.component.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from "react-router-dom";
 
 const ShipmentsList = () => {
   const [shipments, setShipments] = useState([]);
